d27e1f291cde41ebc66c05c763fb1d9b2460b7e5
imcuttle <moyuyc95@gmail.com>
2018-09-18 01:27:55 +0800

    chore: before release

--- packages/psd-to-html/lib/psd-to-hast.js
+++ packages/psd-to-html/lib/psd-to-hast.js
@@ -7,8 +7,8 @@
 const h = require('hastscript')
 const PSD = require('@moyuyc/psd')
 const get = require('lodash.get')
-
 const visit = require('@moyuyc/walk-tree')
+
 const psdUtils = require('./psd-utils')
 
 function assert(check, ...argv) {
